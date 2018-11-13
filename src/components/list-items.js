018520855795246a5c6b842a8c6405d516a7c5d5
Amar <sharewithamar@gmail.com>
2018-11-13 20:48:07 +0530

    single todo-item

--- src/components/list-items.js
+++ src/components/list-items.js
@@ -1,6 +1,6 @@
 import { LitElement, html } from '@polymer/lit-element';
 import { repeat } from 'lit-html/directives/repeat'
-
+import './todo-items';
 
 class ListItems extends LitElement {
 
@@ -17,9 +17,10 @@ class ListItems extends LitElement {
     render() {
         return html`
         <ul>
-        ${repeat(this.todolist, todo => html` <li> ${todo.item}</li>`)}
+        ${repeat(this.todolist, todo => html`<todo-item .todoItem=${todo}></todo-item>`)}
 
         </ul > `
+        //  works     ${repeat(this.todolist, todo => html` <li> ${todo.item}</li>`)} 
         //  const render = () => html`items = ${items.map((i) => `item: ${i}`)}`;
         //  ${repeat(this.todoList, todo => todo.id, todo => html` <li> ${todo.item}</li>`)}
 
